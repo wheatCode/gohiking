9324c2d35cf62538719df950928ac08123c2866a
Ming <ming@Mingde-MacBook-Pro.local>
2021-12-13 20:08:56 +0800

    update trail controller

--- app/Http/Controllers/API/TrailController.php
+++ app/Http/Controllers/API/TrailController.php
@@ -23,8 +23,8 @@ class TrailController extends Controller
         // 篩選欄位條件
         $result=$trail->get();
         if (isset($request->filters)) {
-            $countieFilter='';
             foreach ($filters as $key => $filter) {
+                dd($filters);
                 //迴圈取得所有filter參數
                 switch ($filter) {
                     case 'title':
