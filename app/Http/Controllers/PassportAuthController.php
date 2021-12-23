35c9cc58cdfa5f8acf3ef1a40494fc590036390b
Ming <ming@Mingde-MacBook-Pro.local>
2021-12-24 02:50:01 +0800

    git init

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -149,6 +149,8 @@ class PassportAuthController extends Controller
         if ($validator->fails()) {
             $error = "";
             $errors = $validator->errors();
+            foreach ($errors->all() as $message)
+                $error = $message;
             return response()->json(['error' => $error], 401);
         }
         $findUser = User::where('email', $request->email)->where('verification_code_0', $request->verificationCode0)->where('verification_code_1', $request->verificationCode1)->where('verification_code_2', $request->verificationCode2)->where('verification_code_3', $request->verificationCode3)->first();
