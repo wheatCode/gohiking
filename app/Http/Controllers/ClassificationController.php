67c5a60e647e96879ce65d15cb6442cc67e1308b
Ming <ming@Mingde-MacBook-Pro.local>
2021-12-27 05:37:55 +0800

    update

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -48,9 +48,8 @@ class ClassificationController extends Controller
      */
     public function show($id, Request $request)
     {
-        dd($request);
         $classifications = Classification::with('trails', 'trails.location', 'trails.location.county')->find($id);
-        $userTrail = Favorite::select('trail_id')->where('user_id', '=', $request->uuid)->get();
+        $userTrail = Favorite::select('trail_id')->where('user_id', '=', $id)->get();
         for ($i = 0; $i < count($classifications->trails); $i++) {
             $classifications->trails[$i]["favorite"] = false;
             for ($j = 0; $j < count($userTrail); $j++) {
